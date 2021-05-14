fc31cfa6e08cc903edfa2f3f84c9d08fab9726ea
Nick <nick@nimo.uk>
2021-05-14 22:35:29 +0100

    Initial import

---
+++ README.md
@@ -0,0 +1,3 @@
+# Overview
+microBlinky - umake / nimolib flashing LED demo for the microNIMO board.
+
---
+++ main.c
@@ -0,0 +1,41 @@
+/*
+ * This file is part of the microBlinky distribution (https://github.com/nimo-labs/microBlinky).
+ * Copyright (c) 2021 Nimolabs Ltd. www.nimo.uk
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Microcontroller */
+#include "NuMicro.h"
+
+/* Books */
+#include <gpio.h>
+#include <delay.h>
+
+/* Project configuration */
+#include "nimolib.h"
+#include "system.h"
+
+void main(void)
+{
+    GPIO_PIN_DIR(LED_BLUE_PORT, LED_BLUE_PIN, GPIO_DIR_OUT);
+    GPIO_PIN_OUT(LED_BLUE_PORT, LED_BLUE_PIN, GPIO_OUT_LOW);
+
+    delaySetup(DELAY_BASE_MILLI_SEC); /*Clock timer at 1mS*/
+
+    while (1)
+    {
+        GPIO_PIN_TGL(LED_BLUE_PORT, LED_BLUE_PIN);
+        delayMs(2000);
+    }
+}
