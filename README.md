aed2909557f55a2cb362e70390808f8dabeee9e6
Nick <nick@nimo.uk>
2021-08-30 19:33:06 +0100

    Added build script. Added build badge to readme

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+![Build status](https://github.com/nimo-labs/microNIMO-examples/actions/workflows/build_master.yml/badge.svg)
 # Overview
 [microBlinky](https://nimo.uk/microblinky/) - umake / nimolib flashing LED demo for the microNIMO board.\
 [usbVcom](https://nimo.uk/usb-vcom/) - A basic virtual serial (COM) port example.
---
+++ localBuildall.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+for d in */
+do
+	echo "Processing $d..."
+	cd $d
+	umake clean
+	umake
+	make
+	if [ $? -eq 0 ]; then
+		echo -------------------
+    	echo Build of $d passed
+		echo -------------------
+		cd ..
+	else
+    	echo build of $d failed
+		exit 1
+	fi
+done
+echo -------------------
+echo All builds completed successfully
+echo -------------------
